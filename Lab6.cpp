3974d3186f93b5e02291dc8c15861d56346b8da5
Руслан <@DESKTOP-V4KRFG3>
2022-11-11 15:45:16 +0300

    zad1

--- Lab6.cpp
+++ Lab6.cpp
@@ -1,54 +1,21 @@
-﻿//getchar
-//putchar
+﻿//С клавиатуры ввести строку. Подсчитать в ней количество символов и каждый пятый символ заменить на «#»
 #include <iostream>
-#include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
-#include <random>
-#include <cstring>
-#include <iomanip>
-#include <string.h>
+#include <string>
+#include <cstdio>
+#include <algorithm>
 using namespace std;
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    char a[80], c[3], d[] = "под", b[20][20];
-    int i, j, str, stb, k = 0;
-    cout << "Введите предложение:" << endl;
-    cin >> a;
-    cout << a;
-    for (i = 0, str = stb = 0; i < strlen(a); i++)
-    {
-        if (a[i] != ' ')
-        {
-            b[str][stb] = a[i]; stb++;
-        }
-        else
-        {
-            b[str][stb] = '\0';
-            str++; 
-            stb = 0;
-        }
-    }
-    b[str][stb] = '\0';
-    cout << "Строковый массив" << endl;
-    for (i = 0; i <= str; i++)
-    {
-        puts(b[i]);
-    }
-    //getch();
-    /*for (i = 0; i <= str; i++)
-    {
-        strncpy(c, b[i], 3);
-        c[3] = '\0';
-        if (strcmp(c, d) == 0)
-        {
-            k++;
-        }
-    }
-    cout << k << endl;*/
-    //getch();
+	setlocale(LC_ALL, "Russian");
+	string a;
+	int i;
+	cout << "Введите предложение: " << endl;
+	cin >> a;
+	cout << "Количество символов: " << a.length() << endl;
+	for (i = 4; a.length() > i; i += 5)
+	{
+		a.insert(i, "#");
+	}
+	cout << "Изменённое предложение: " << a << endl;
+	//s.empty() — возвращает значение true, если строка s пуста, false — в противном случае;
 }
-
-
-
