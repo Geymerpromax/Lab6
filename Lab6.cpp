81a2e1f705be3824f86e40ae677c288cca763570
Руслан <@DESKTOP-V4KRFG3>
2022-11-17 19:58:42 +0300

    еп

--- Lab6.cpp
+++ Lab6.cpp
@@ -1,18 +1,35 @@
-﻿//С клавиатуры ввести строку. Подсчитать в ней количество символов и каждый пятый символ заменить на «#»
-#include <iostream>
+﻿#include <iostream>
 #include <cstring>
-#include <algorithm>
+#include <iomanip>
 #include <cstdio>
 #include <stdlib.h>
+
 using namespace std;
 int main()
 {
 	system("chcp 1251");
 	system("cls");
-	char a[256];
+	char a[256], b[64];
 	int i;
-	cout << "Введите предложение: ";
-	cin.getline (a, 256, '*');
-	cout << "Предложение: " << a << endl;
-	cout << strlen(a); // длина строки
+
+	cout << "Введите предложение 1: ";
+	cin.getline (a, 256);
+	cout << "Введите предложение 2: ";
+	cin.getline(b, 64);
+
+	cout << "Предложение 1: " << a << endl; 
+	cout << "Предложение 2: " << b << endl;
+
+	cout << "Размер 1: " << strlen(a) << endl; // длина строки
+
+	strcat_s(a, 256, b);// a + b 
+	cout << "Изм. a: " << a << endl;
+	strcat_s(a, 256, "ggg");// a + "ggg" 
+	cout << "Изм. a: " << a << endl;
+
+	strcpy_s(a, 256, b);// a = b 
+	cout << "Изм. a: " << a << endl; 
+
+	cout << strcmp(a, b);// от слова compare – сравнение
+
 }
